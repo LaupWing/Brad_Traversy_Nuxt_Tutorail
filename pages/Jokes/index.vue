8942227f2d6f119a574f34c8cca7d039d2fece1c
LaupWing <loc_strict@hotmail.com>
2020-12-27 15:53:09 +0100

    added axios

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -5,11 +5,40 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+   data(){
+      return{
+         jokes: []
+      }
+   },
+   async created(){
+      const config = {
+         headers:{
+            'Accept': 'application/json'
+         }
+      }
+      try{
+         const res = await axios.get('https://icanhazdadjoke.com/search', config)
+         const json = await res.json()
+         console.log(json)
+      }catch(e){
+         console.log(e)
+      }
+
+   },
+   head(){
+      return{
+         title: 'Dad Jokes',
+         meta:[
+            {
+               hid: 'description',
+               name: 'description',
+               content: 'Best place for corny dad jokes'
+            }
+         ]
+      }
+   }
 }
 </script>
-
-<style>
-
-</style>
